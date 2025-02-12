c28e5972b7f45e1a7138c723d504257b6fc1fb90
Erik Landolsi <erik@pixw.se>
2025-02-12 12:15:28 -0500

    Added more lab functions to look at counts, analyze row/column means/normalization etc

--- lab_10x_cellpooling.py
+++ lab_10x_cellpooling.py
@@ -22,7 +22,6 @@ def lab_10x_pooling():
 
 def lab_read_visium():
     # Read the Visium HD the same way that they do in the example
-
     input_path = '/media/erik/T9/run2_A/outs/binned_outputs/square_002um/'
 
     # Load Visium HD data and spatial coordinates of spots
@@ -42,54 +41,6 @@ def lab_read_visium():
     geometry = [Point(xy) for xy in zip(df_pos['pxl_col_in_fullres'], df_pos['pxl_row_in_fullres'])]
     gdf_coordinates = gpd.GeoDataFrame(df_pos, geometry=geometry)
 
-
-def lab_read_visium2():
-    # Read the Visium HD using scanpy and play around a bit
-    # TODO: Move to some scanpy_lab file!
-
-    plt.interactive(True)
-
-    input_path = '/media/erik/T9/run2_A/outs/binned_outputs/square_002um/'
-
-    # Load Visium HD data and spatial coordinates of spots
-    df_pos = pd.read_parquet(os.path.join(input_path, 'spatial/tissue_positions.parquet'))
-    adata = sc.read_10x_h5(os.path.join(input_path, 'filtered_feature_bc_matrix.h5'))
-
-    adata.X
-    # adata.X is now a CSR matrix with shape 10732334x18085
-    # Here, 18085 is the number of genes and 10732334 is some form of
-    # encoded 
-
-    counts = adata.X.sum(1)  # Sum over columns
-
-    # Reshape to an image (should not be done like this, but just to
-    # understand the structure)
-
-    # This is not the right way!
-    # There is obs_names and var_names that are connected to the
-    # rows/columns (columns/rows?) in X. We need to extract the right
-    # spot name and map to corresponding x/y coordinate. This
-    # coordinate is also encoded in the spot name (but we should perhaps not
-    # rely on that).
-
-    img = np.zeros((3350, 3350))
-    for ix, c in enumerate(counts):
-        x = ix // 3350
-        y = ix % 3350 
-        # TODO: Or the other way around
-        
-        nc = c.item() / 10.0
-        nc = min(nc, 1.0)
-        img[y, x] = nc
-    
-    img_u8 = (img * 255).astype(np.uint8)
-    img_pil = PIL.Image.fromarray(img_u8)
-    img_pil.save('_temp/temp.png')
-
-    plt.imshow(img)
-    plt.waitforbuttonpress()
-
-    print('Done!')
 
 
 if __name__ == '__main__':
--- lab_cll2a_st_image.py
+++ lab_cll2a_st_image.py
@@ -12,11 +12,27 @@ from scipy import sparse
 import scanpy as sc
 import pandas as pd
 
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
 import cv2
+import PIL
 
+# Helper functions
+
+def spotname_to_xy(name: str) -> tuple[int, int]:
+    # TODO: Error handling
+    # TODO: Don't assume these spot names, should use the tissue_positions instead
+
+    parts = name.split('_') # e.g. ['s', '002um', '00658', '01498-1']
+   
+    y = int(parts[2])
+    parts2 = parts[3].split('-') # e.g. ['01498', '1']
+    x = int(parts2[0])
+
+    return (x,y)
+    
 
 def lab_read_parquet():
     # Reads the tissue_positions parquet file
@@ -81,20 +97,207 @@ def lab_overlay_bin_centers():
         plt.pause(60)
 
 
-def show_total_counts():
-    # TODO: Show total read counts for each cell
+def lab_counts_as_image():
+    # Read the Visium HD using scanpy, sum all counts and visualize it as an
+    # image, to understand how the datastructure works
 
-    input_path = '/media/erik/T9/run2_A/outs/binned_outputs/square_002um'
+    plt.interactive(True)
+    input_path = '/media/erik/T9/run2_A/outs/binned_outputs/square_002um/'
 
-    adata = sq.read.visium(
-        input_path,
-        counts_file='filtered_feature_bc_matrix.h5')
+    # Load Visium HD data and spatial coordinates of spots
+    df_pos = pd.read_parquet(os.path.join(input_path, 'spatial/tissue_positions.parquet'))
+    adata = sc.read_10x_h5(os.path.join(input_path, 'filtered_feature_bc_matrix.h5'))
+
+    adata.X
+    # adata.X is now a CSR matrix with shape 10732334x18085
+    # Here, 18085 is the number of genes and 10732334 is the numer of spots. To
+    # find the spatial location of each spot, we need to look at the
+    # corresponding spot name or spot data in the parquet file.
+
+    counts = adata.X.sum(1)  # Sum over columns
+
+    # Put all counts in an image-like structure (slow, not the right way later
+    # on, but to understand the structure)
+    max_count = 0
+    img = np.zeros((3350, 3350))
+    for ix, c in enumerate(counts):
+
+        if ix % 100000 == 0:
+            print(f'Processed spots: {ix//100000}00k, max = {max_count}')
+
+        max_count = max(c, max_count)
+
+        # Find x,y coordinate
+        spot_name = adata.obs_names[ix]
+        x, y = spotname_to_xy(spot_name)
+
+        # Basic normalization
+        nc = c.item() / 80.0
+        nc = min(nc, 1.0)
+
+        # Set the pixel
+        img[y, x] = nc
+
+    print(f'max_count = {max_count}')
+
+    # Convert to u8 and save
+    img_u8 = (img * 255).astype(np.uint8)
+    img_pil = PIL.Image.fromarray(img_u8)
+    img_pil.save('_temp/temp.png')
+
+    print('Done!')
 
 
+def save_total_counts_image():
+    # Read the Visium HD using scanpy, sum all counts and save it directly
+    # as a png image, where the pixel value is the total count (max 132, so
+    # works fine)
 
+    input_path = '/media/erik/T9/run2_A/outs/binned_outputs/square_002um/'
+
+    # Load Visium HD data
+    adata = sc.read_10x_h5(os.path.join(input_path, 'filtered_feature_bc_matrix.h5'))
+
+    counts = adata.X.sum(1)  # Sum over columns
+
+    # Put all counts in an image-like structure (slow, not the right way later
+    # on, but to understand the structure)
+    max_count = 0
+    img = np.zeros((3350, 3350), dtype=np.uint8)
+    for ix, count in enumerate(counts):
+
+        if ix % 100000 == 0:
+            print(f'Processed spots: {ix//100000}00k, max = {max_count}')
+
+        max_count = max(count, max_count)
+
+        # Find x,y coordinate
+        spot_name = adata.obs_names[ix]
+        x, y = spotname_to_xy(spot_name)
+
+        # Set the pixel
+        img[y, x] = count
+
+    print(f'max_count = {max_count}')
+
+    # Convert to u8 and save
+    img_pil = PIL.Image.fromarray(img)
+    img_pil.save('_temp/temp_raw_u8.png')
+
+    print('Done!')
+
+
+def save_active_genes_image():
+    # Read the Visium HD using scanpy, sum threshold at 1 (such that we only
+    # measure the presence/absence of a specific gene at a specific location).
+    # Then sum over all genes and save the result
+
+    input_path = '/media/erik/T9/run2_A/outs/binned_outputs/square_002um/'
+
+    # Load Visium HD data
+    adata = sc.read_10x_h5(os.path.join(input_path, 'filtered_feature_bc_matrix.h5'))
+    
+    adata.X[adata.X >= 1] = 1
+    counts = adata.X.sum(1)  # Sum over columns
+
+    # Put all counts in an image-like structure (slow, not the right way later
+    # on, but to understand the structure)
+    max_count = 0
+    img = np.zeros((3350, 3350), dtype=np.uint8)
+    for ix, count in enumerate(counts):
+
+        if ix % 100000 == 0:
+            print(f'Processed spots: {ix//100000}00k, max = {max_count}')
+
+        max_count = max(count, max_count)
+
+        # Find x,y coordinate
+        spot_name = adata.obs_names[ix]
+        x, y = spotname_to_xy(spot_name)
+
+        # Set the pixel
+        img[y, x] = count
+
+    print(f'max_count = {max_count}')
+
+    # Convert to u8 and save
+    img_pil = PIL.Image.fromarray(img)
+    img_pil.save('_temp/temp_thresholded_raw.png')
+
+    print('Done!')
+
+
+def lab_xy_normalize():
+    input_file = '_temp/2A_counts_raw.png'
+
+    img = cv2.imread(input_file, cv2.IMREAD_GRAYSCALE)
+
+    mean_rows = np.mean(img, axis=0, keepdims=True) # Mean over rows
+    mean_cols = np.mean(img, axis=1, keepdims=True) # Mean over cols
+
+    img_f = img / mean_rows
+    img_f = img_f / mean_cols
+
+    figure, axis = plt.subplots(2, 1)
+
+    if False:
+        # Save normalized image
+        img_u8 = (img_f * 255).astype(np.uint8)
+        img_pil = PIL.Image.fromarray(img_u8)
+        img_pil.save('_temp/temp_rowcol_normalized.png')
+
+    if True:
+        # Plot row/col means
+
+        mean_rows = mean_rows.flatten()
+        mean_cols = mean_cols.flatten()
+
+        mean_rows = np.sort(mean_rows)
+        mean_cols = np.sort(mean_cols)
+
+        axis[0].plot(mean_rows)
+        axis[0].set_title('mean over rows')
+
+        axis[1].plot(mean_cols)
+        axis[1].set_title('mean over columns')
+
+        figure.show()
+        figure.waitforbuttonpress()
+
+
+def lab_nof_active_genes():
+
+    input_path = '/media/erik/T9/run2_A/outs/binned_outputs/square_002um/'
+
+    # Load Visium HD data
+    adata = sc.read_10x_h5(os.path.join(input_path, 'filtered_feature_bc_matrix.h5'))
+
+    counts = adata.X.sum(1)  # Sum over columns
+    hist_counts, hist_bins = np.histogram(adata.X.data, bins=np.arange(30))
+
+    total_sum = np.sum(hist_counts)
+    hist_rates = hist_counts / total_sum
+
+    nof_1s = hist_rates[1]
+    nof_2s = hist_rates[2]
+    nof_3s = hist_rates[3]
+    print(f'Rate of 1s: {nof_1s*100:.02f}%')
+    print(f'Rate of 2s: {nof_2s*100:.02f}%')
+    print(f'Rate of 3s: {nof_3s*100:.02f}%')
+    # Conclusion: On locations where genes are found, it's typically just one
+    # (99.38% of all times). In 0.59% of all times, there's two. 
+
+    print('OK')
 
 
 if __name__ == '__main__':
     # lab_read_parquet()
     # lab_read_visium()
-    lab_overlay_bin_centers()
+    # lab_overlay_bin_centers()
+    # lab_counts_as_image()
+
+    # save_total_counts_image()
+    # lab_xy_normalize()
+
+    # lab_nof_active_genes()
+    save_active_genes_image()
